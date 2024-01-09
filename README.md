e5b2e58308b6cd87ef170a97b03fa9f970893810
meltyfruits <ts.tunney01@gmail.com>
2024-01-08 18:23:03 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PkmnRTM
+[Pokémon Readable Type Matchups](https://mintyfruits.github.io/PkmnRTM/)
+
+A simple tool that quickly shows what typing is super effective against another given type (e.g., Water > Fire). 
+
+# To do
+* [x] Finish type matchups
+* [x] Add changelog to main screen
+* [ ] Create solution for dual type weaknesses
+* [ ] Develop icon switching
+* [ ] Add "strong against"
+
+# References
+* Icons from <https://archives.bulbagarden.net/wiki/Category:Type_icons>
---
+++ js/types.js
@@ -0,0 +1,166 @@
+function normalType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('normal');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function fireType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('fire');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function waterType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('water');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function electricType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('electric');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function grassType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('grass');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function iceType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('ice');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function fightingType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('fighting');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function poisonType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('poison');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function groundType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('ground');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function flyingType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('flying');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function psychicType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('psychic');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function bugType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('bug');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function rockType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('rock');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function ghostType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('ghost');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function dragonType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('dragon');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function darkType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('dark');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function steelType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('steel');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function fairyType() {
+  var x = document.getElementById('typeTable');
+  var y = document.getElementById('fairy');
+  x.style.visibility = 'hidden';
+  y.style.visibility = 'visible';
+}
+
+function reset() {
+  var x = document.getElementById('typeTable');
+  var normal = document.getElementById('normal');
+  var fire = document.getElementById('fire');
+  var water = document.getElementById('water');
+  var electric = document.getElementById('electric');
+  var grass = document.getElementById('grass');
+  var ice = document.getElementById('ice');
+  var fighting = document.getElementById('fighting');
+  var poison = document.getElementById('poison');
+  var ground = document.getElementById('ground');
+  var flying = document.getElementById('flying');
+  var psychic = document.getElementById('psychic');
+  var bug = document.getElementById('bug');
+  var rock = document.getElementById('rock');
+  var ghost = document.getElementById('ghost');
+  var dragon = document.getElementById('dragon');
+  var dark = document.getElementById('dark');
+  var steel = document.getElementById('steel');
+  var fairy = document.getElementById('fairy');
+  x.style.visibility = 'visible';
+  normal.style.visibility = 'hidden';
+  fire.style.visibility = 'hidden';
+  water.style.visibility = 'hidden';
+  electric.style.visibility = 'hidden';
+  grass.style.visibility = 'hidden';
+  ice.style.visibility = 'hidden';
+  fighting.style.visibility = 'hidden';
+  poison.style.visibility = 'hidden';
+  ground.style.visibility = 'hidden';
+  flying.style.visibility = 'hidden';
+  psychic.style.visibility = 'hidden';
+  bug.style.visibility = 'hidden';
+  rock.style.visibility = 'hidden';
+  ghost.style.visibility = 'hidden';
+  dragon.style.visibility = 'hidden';
+  dark.style.visibility = 'hidden';
+  steel.style.visibility = 'hidden';
+  fairy.style.visibility = 'hidden';
+}
